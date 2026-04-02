6bba2914d0ecbc7e56255a14e5d607e57ca2408b
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-04-02 23:30:02 +0530

    console.log in js

---
+++ 01.js
@@ -0,0 +1,14 @@
+let a = 10;
+let firstName = '张三';
+let isStudent = true;
+
+console.log(a);
+console.log(firstName);
+console.log(isStudent);
+// //console.log() is used to print output so you can see what your code is doing.
+
+////👉 It prints data inside the browser console (Inspect → Console tab)
+
+let d = 10,
+  b = 5;
+console.log(d + b);
