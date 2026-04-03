65faaf43b4f4d7815e10f621b45c3bed61750701
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-04-03 16:08:25 +0530

    Add initial JavaScript examples for functions, arithmetic, and object literals

---
+++ 07.js
@@ -0,0 +1,25 @@
+const greet = (name) => {
+  console.log(name);
+};
+greet('John');
+
+const add = (a, b) => {
+  return a + b;
+};
+console.log(add(78, 32));
+
+// ? const add=(a,b)=>a+b;
+
+const square = (x) => x * x;
+console.log(square(6));
+
+const sayHi = () => console.log('Hi');
+sayHi();
+
+const getUser = () => {
+  name: 'Hammad';
+};
+console.log(getUser());
+
+const getUser1 = () => ({ name: 'Adnaan' });
+console.log(getUser1());
