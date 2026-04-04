4b27ae41a3c0598728a00db43c2366e9ede91fa5
Mohamamd Hammad <hammad.scripted@gmail.com>
2026-04-04 09:32:00 +0530

    arrays in js

---
+++ 09.js
@@ -0,0 +1,24 @@
+let colors = ['red', 'blue', 'green'];
+let personalDetails = ['hammad', 21, false];
+console.log(personalDetails);
+console.log(colors);
+
+let arr = new Array(3);
+console.log(arr);
+// it create empty slots for the array elements
+
+// ! typeof array is not “Array”, it’s an “Object”.
+// ❑ Array is a special type of object
+// ❑ Arrays are mutable.
+console.log(typeof colors);
+
+let arr1 = [1, 2];
+arr1.push(3);
+console.log(arr1);
+
+const fruits = ['apple', 'banana'];
+let myList = fruits.toString();
+console.log(myList);
+// The toString() method returns the elements of an array as a comma separated string.
+
+//  for reference-https://www.w3schools.com/js/js_array_methods.asp
